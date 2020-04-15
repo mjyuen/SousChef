6103aec475b6acf66a9a5970a6ec750fefe1f12d
Michelle Yuen <mjyuen@princeton.edu>
2020-04-14 21:39:55 -0400

    test out drag drop functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { DragDropContainer, DropTarget } from 'react-drag-drop-container';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <DragDropContainer targetKey="foo">
+            <div>Drag Me!</div>
+        </DragDropContainer>
+
+        <DropTarget targetKey="foo" onHit={(e) => { console.log(e); alert("hello im dumb")}}>
+            <p>I'm a valid drop target for the object above since we both have the same targetKey!</p>
+        </DropTarget>
       </header>
     </div>
   );
